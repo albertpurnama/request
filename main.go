d587237e27c436fb9101cfe8b7018ddcc99d183a
Albert Putra Purnama <albertputrapurnama@gmail.com>
2018-07-10 18:48:32 +0700

    added MakeEtherTransaction function

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/parnurzeal/gorequest"
@@ -13,6 +15,7 @@ import (
 const CREATE_NEW_ADDR_URL = "https://api.blockcypher.com/v1/beth/test/addrs?token=e7066438be7f48d1b08a325450d31694"
 const CREATE_NEW_TX_URL = "https://api.blockcypher.com/v1/beth/test/txs/new?token=e7066438be7f48d1b08a325450d31694"
 const FAUCET_URL = "https://api.blockcypher.com/v1/beth/test/faucet?token=e7066438be7f48d1b08a325450d31694"
+const SEND_TX_URL = "https://api.blockcypher.com/v1/beth/test/txs/send?token=e7066438be7f48d1b08a325450d31694"
 
 const (
 	BETH_ADDR        = "3c689107595e512e5d0d01eaeb32ac345e928980"
@@ -23,39 +26,103 @@ const (
 	BETH_PUBLIC_TWO  = "04ac417ab0a3ad18006329701b0c8e82cc6eda4787d62dd23166650f0025bb512f7700250c94f05456aa7b1c89e524532f063bd0fd15f94d93b710c2dc3f8fad8e"
 )
 
+type EtherTX struct {
+	BlockHeight int       `json:"block_height" binding:"required"`
+	BlockIndex  uint      `json:"block_index" `
+	Hash        string    `json:"hash" binding:"required"`
+	Addresses   []string  `json:"addresses" binding:"required"`
+	Total       uint      `json:"total" binding:"required"`
+	Fees        uint      `json:"fees" binding:"required"`
+	Size        uint      `json:"size" binding:"required"`
+	GasUsed     uint      `json:"gas_used"`
+	GasLimit    uint      `json:"gas_limit"`
+	GasPrice    uint      `json:"gas_price" binding:"required"`
+	Received    time.Time `json:"received" binding:"required"`
+	Version     int       `json:"ver" binding:"required"`
+	DoubleSpend bool      `json:"double_spend" binding:"required"`
+	VinSz       uint      `json:"vin_sz" binding:"required"`
+	VoutSz      uint      `json:"vout_sz" binding:"required"`
+	Inputs      []struct {
+		Sequence  uint     `json:"sequence"`
+		Addresses []string `json:"addresses"`
+	} `json:"inputs" binding:"required"`
+	Outputs []struct {
+		Value     uint     `json:"value"`
+		Script    string   `json:"script"`
+		Addresses []string `json:"addresses"`
+	} `json:"outputs" binding:"required"`
+	Confirmed time.Time `json:confirmed`
+	BlockHash string    `json:block_hash`
+	ExecError string    `json:execution_error`
+}
+
+type EtherTxSkeleton struct {
+	Tx         EtherTX  `json:"tx" binding:"required"`
+	Tosign     []string `json:"tosign" binding:"required"`
+	Signatures []string `json:"signatures"`
+	Errors     []struct {
+		Error string `json:"error"`
+	} `json:"errors"`
+}
+
 func main() {
-	var dat map[string]interface{}
+	// var dat EtherTxSkeleton
 	request := gorequest.New()
-	resp, body, _ := request.Post(CREATE_NEW_TX_URL).
-		Send(CreatePartiallyFilledTX(BETH_ADDR, BETH_ADDR_TWO, 1)).
-		End()
 
-	if resp.StatusCode != 200 && resp.StatusCode != 201 {
-		log.Fatal(body)
-		return
-	}
-	json.Unmarshal([]byte(body), &dat)
-	fmt.Println(dat["tosign"].([]interface{})[0])
-
-	str, err := Sign(BETH_PRIVATE, dat["tosign"].([]interface{})[0].(string))
+	// Making transaction
+	data, err := MakeEtherTransaction(BETH_ADDR, BETH_ADDR_TWO, 15, request)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Println(str)
+	fmt.Println(data)
+	// *************
+
+	// resp, body, _ := request.Post(CREATE_NEW_TX_URL).
+	// 	Send(CreatePartiallyFilledTX(BETH_ADDR, BETH_ADDR_TWO, 1)).
+	// 	End()
+
+	// if resp.StatusCode != 200 && resp.StatusCode != 201 {
+	// 	log.Fatal(body)
+	// 	return
+	// }
+	// fmt.Println(body)
+	// json.Unmarshal([]byte(body), &dat)
+
+	// str, err := Sign(BETH_PRIVATE, dat.Tosign[0])
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	return
+	// }
+	// fmt.Println(str)
+
+	// dat.Signatures = append(dat.Signatures, str)
+
+	// abc, _ := json.Marshal(dat)
+	// fmt.Println(string(abc))
+
+	// resp, body, _ = request.Post(SEND_TX_URL).
+	// 	Send(string(abc)).
+	// 	End()
+	// if resp.StatusCode != 200 && resp.StatusCode != 201 {
+	// 	log.Fatal(body)
+	// 	return
+	// }
+	// fmt.Println(body)
 
 	// // faucet
 	// resp, body, _ := request.Post(FAUCET_URL).
-	// 	Send(str).
+	// 	Send(CreateFaucetString(BETH_ADDR, 1e18)).
 	// 	End()
 	// if resp.StatusCode != 200 {
+	// 	fmt.Println(resp)
 	// 	log.Fatal(CreateFaucetString(BETH_ADDR, 1e18))
 	// 	return
 	// }
 	// fmt.Println(body)
 
 	// // get balance
-	// resp, body, _ := request.Get(GetETHBalanceURL(BETH_ADDR)).
+	// resp, body, _ := request.Get(GetETHBalanceURL(BETH_ADDR_TWO)).
 	// 	End()
 	// if resp.StatusCode != 200 {
 	// 	fmt.Println(body)
@@ -65,6 +132,50 @@ func main() {
 	// fmt.Println(body)
 }
 
+// MakeEtherTransaction makes Ethereum transaction
+func MakeEtherTransaction(sender string, receiver string, amount float64, r *gorequest.SuperAgent) (EtherTxSkeleton, error) {
+	// Create a new empty template for handling API calls payload
+	var dat EtherTxSkeleton
+
+	// Create initial TX structure using BlockCypher's TX endpoints
+	resp, body, _ := r.Post(CREATE_NEW_TX_URL).
+		Send(CreatePartiallyFilledTX(sender, receiver, amount)).
+		End()
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		log.Fatal(body)
+		return EtherTxSkeleton{}, errors.New("Error creating initial unsigned transaction")
+	}
+
+	json.Unmarshal([]byte(body), &dat)
+
+	// Sign the initial transaction's tosign
+	str, err := Sign(BETH_PRIVATE, dat.Tosign[0])
+	if err != nil {
+		log.Fatal(err)
+		return EtherTxSkeleton{}, errors.New("Error signing transaction")
+	}
+
+	dat.Signatures = append(dat.Signatures, str)
+
+	signedTxSkel, _ := json.Marshal(dat)
+
+	// Send the initial transaction with signature
+	resp, body, _ = r.Post(SEND_TX_URL).
+		Send(string(signedTxSkel)).
+		End()
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		log.Fatal(body)
+		return EtherTxSkeleton{}, errors.New("Error sending signed transaction")
+	}
+
+	json.Unmarshal([]byte(body), &dat)
+
+	return dat, nil
+}
+
+// Sign functions signs the transaction with the main account's private
+// key
 func Sign(private string, data string) (str string, err error) {
 	dat, err := hex.DecodeString(data)
 	if err != nil {
